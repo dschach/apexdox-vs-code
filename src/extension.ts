16dbfa920ac8e7e4ae91d69d622c68dd07fa8121
David Schach <dschach@x2od.com>
2022-05-30 17:25:40 -0700

    test import update

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,17 @@
 import disposables, { ApexDoxCommand } from './disposables/index';
 import { closeServer } from './engine/server';
-import { ExtensionContext } from 'vscode';
+//import { ExtensionContext } from 'vscode';
+import * as vscode from 'vscode';
 
 export const EXTENSION = 'apexdox';
 export const REPOSITORY = 'https://github.com/no-stack-dub-sack/apexdox-vs-code';
 
-export function activate(context: ExtensionContext) {
-	disposables.forEach((cmd: ApexDoxCommand) => {
-		context.subscriptions.push(cmd(context));
-	});
+export function activate(context: vscode.ExtensionContext) {
+  disposables.forEach((cmd: ApexDoxCommand) => {
+    context.subscriptions.push(cmd(context));
+  });
 }
 
 export function deactivate() {
-	closeServer();
+  closeServer();
 }
