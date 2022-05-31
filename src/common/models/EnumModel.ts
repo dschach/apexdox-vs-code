73a5dd8c96c9f05e4685a668a9d5ede15edab463
David Schach <dschach@x2od.com>
2022-05-30 17:25:07 -0700

    Formatting changes (pt2)

--- src/common/models/EnumModel.ts
+++ src/common/models/EnumModel.ts
@@ -3,48 +3,47 @@ import { ModelType, TopLevelModel } from './TopLevelModel';
 import { Option } from '../..';
 
 class EnumModel extends TopLevelModel {
+  private _values: string[] = [];
 
-    private _values: string[] = [];
+  public constructor(comments: string[], nameLine: string, lineNum: number, sourceUrl: Option<string>) {
+    super(comments, ModelType.ENUM, sourceUrl);
+    this.setNameLine(nameLine, lineNum);
+    this.setValues(nameLine);
+  }
 
-    public constructor(comments: string[], nameLine: string, lineNum: number, sourceUrl: Option<string>) {
-        super(comments, ModelType.ENUM, sourceUrl);
-        this.setNameLine(nameLine, lineNum);
-        this.setValues(nameLine);
+  public get groupName(): string {
+    return this._groupName;
+  }
+
+  public get name(): string {
+    let nameLine = this.nameLine;
+    let i = nameLine.indexOf(ApexDox.ENUM);
+    return nameLine.slice(i + ApexDox.ENUM.length).trim();
+  }
+
+  public get values(): string[] {
+    return this._values;
+  }
+
+  protected setNameLine(nameLine: string, lineNum: number): void {
+    if (nameLine) {
+      // remove any trailing stuff after enum name
+      let i = nameLine.indexOf('{');
+      if (i > 0) {
+        nameLine = nameLine.substring(0, i);
+      }
     }
 
-    public get groupName(): string {
-        return this._groupName;
-    }
+    super.setNameLine(nameLine.trim(), lineNum);
+  }
 
-    public get name(): string {
-        let nameLine = this.nameLine;
-        let i = nameLine.indexOf(ApexDox.ENUM);
-        return nameLine.slice(i + ApexDox.ENUM.length).trim();
-    }
-
-    public get values(): string[] {
-        return this._values;
-    }
-
-    protected setNameLine(nameLine: string, lineNum: number): void {
-        if (nameLine) {
-            // remove any trailing stuff after enum name
-            let i = nameLine.indexOf('{');
-            if (i > 0) {
-                nameLine = nameLine.substring(0, i);
-            }
-        }
-
-        super.setNameLine(nameLine.trim(), lineNum);
-    }
-
-    private setValues(line: string): void {
-        line = line.substring(line.indexOf('{') + 1, line.indexOf('}'));
-        line.trim()
-            .split(',')
-            .forEach(value => value.trim()
-                && this.values.push(value.trim()));
-    }
+  private setValues(line: string): void {
+    line = line.substring(line.indexOf('{') + 1, line.indexOf('}'));
+    line
+      .trim()
+      .split(',')
+      .forEach((value) => value.trim() && this.values.push(value.trim()));
+  }
 }
 
 export { EnumModel };
--- src/common/models/PropertyModel.ts
+++ src/common/models/PropertyModel.ts
@@ -2,41 +2,40 @@ import { ApexModel } from './ApexModel';
 import { Option } from '../..';
 
 class PropertyModel extends ApexModel {
+  public constructor(comments: string[], nameLine: string, lineNum: number, sourceUrl: Option<string>) {
+    super(comments, sourceUrl);
+    this.setNameLine(nameLine, lineNum);
+  }
 
-    public constructor(comments: string[], nameLine: string, lineNum: number, sourceUrl: Option<string>) {
-        super(comments, sourceUrl);
-        this.setNameLine(nameLine, lineNum);
+  public get name(): string {
+    let nameLine = this.nameLine.trim();
+    if (nameLine) {
+      let lastIndex = nameLine.lastIndexOf(' ');
+      if (lastIndex >= 0) {
+        let propertyName = nameLine.substring(lastIndex + 1);
+        return propertyName;
+      }
+    }
+    return '';
+  }
+
+  protected setNameLine(nameLine: string, lineNum: number): void {
+    if (nameLine) {
+      // remove any trailing stuff after property name. { =
+      let i = nameLine.indexOf('{');
+      if (i === -1) {
+        i = nameLine.indexOf('=');
+      }
+      if (i === -1) {
+        i = nameLine.indexOf(';');
+      }
+      if (i >= 0) {
+        nameLine = nameLine.substring(0, i);
+      }
     }
 
-    public get name(): string {
-        let nameLine = this.nameLine.trim();
-        if (nameLine) {
-            let lastIndex = nameLine.lastIndexOf(' ');
-            if (lastIndex >= 0) {
-                let propertyName = nameLine.substring(lastIndex + 1);
-                return propertyName;
-            }
-        }
-        return '';
-    }
-
-    protected setNameLine(nameLine: string, lineNum: number): void {
-        if (nameLine) {
-            // remove any trailing stuff after property name. { =
-            let i = nameLine.indexOf('{');
-            if (i === -1) {
-                i = nameLine.indexOf('=');
-            }
-            if (i === -1) {
-                i = nameLine.indexOf(';');
-            }
-            if (i >= 0) {
-                nameLine = nameLine.substring(0, i);
-            }
-        }
-
-        super.setNameLine(nameLine, lineNum);
-    }
+    super.setNameLine(nameLine, lineNum);
+  }
 }
 
 export { PropertyModel };
